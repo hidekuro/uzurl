641791c1eb7827e9be706c43e96e74e36919eb8e
hidekuro <hidekuro@gmail.com>
2023-03-18 15:59:38 +0900

    refactor: pint formatting

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,5 @@
 <?php
 
-use App\Http\Controllers\UrlController;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
